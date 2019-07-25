203151ee6d194cf066d31d4bf1be2a5ee2975705
oginyon <s1170096@gmail.com>
2019-07-26 01:20:53 +0900

    削除機能実装

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,33 +1,40 @@
 // Enable chromereload by uncommenting this line:
 // import 'chromereload/devonly'
 
-const DUMMY_URIS = {
-    'https://developer.mozilla.org/ja/docs/Web/API/GlobalEventHandlers/onload': 'window.onload',
-    'https://qiita.com/sqrtxx/items/19fd2114430e9e1fb57f':'Chrome Extension を作って公開する',
-    'https://aws.amazon.com/jp/amplify/': 'AWS Amplify（アプリケーションの構築とデプロイ）| AWS',
-    'https://reactjs.org/docs/react-without-jsx.html': 'React Without JSX',
-    'https://qiita.com/nabepon/items/87bb3b4f1e7bfa342489': 'ReactやVueのJSXについて曖昧に理解する - Qiita'
-};
 window.onload = function() {
-    let storedUriList = JSON.parse(localStorage.getItem('storedUris'));
-    // addLinkList(DUMMY_URIS);
-    addLinkList(storedUriList);
+    window.si
+    let storedUris = JSON.parse(localStorage.getItem('storedUris'));
+    viewList(storedUris);
 };
 
-function addLinkList(uris) {
+function viewList(uris) {
     let list = document.getElementById('read-list');
     for (let key in uris) {
         let uri = uris[key].uri;
         let title = uris[key].title;
         let anchor = document.createElement('a');
-        let removeIcon = document.createElement('i');
-        let button = document.createElement('button');
+        let deleteButton = document.createElement('a');
+        let deleteIcon = document.createElement('i');
+        deleteIcon.setAttribute('class', 'material-icons right');
+        deleteIcon.innerHTML = 'clear';
+        deleteButton.appendChild(deleteIcon);
+        deleteButton.setAttribute('class', 'btn-floating btn-small red');
         anchor.href = uri;
         anchor.innerHTML = title;
         anchor.setAttribute('class', 'collection-item');
+        anchor.setAttribute('id', key);
+        deleteButton.setAttribute('data-key', key);
+        deleteButton.addEventListener('click', function(e) {
+            let key = this.getAttribute('data-key');
+            delete uris[key];
+            localStorage.setItem('storedUris', JSON.stringify(uris));
+            list.removeChild(this);
+            list.removeChild(document.getElementById(key));
+        });
         anchor.addEventListener('click', function(e) {
             chrome.tabs.create({url: uri});
         }, false);
+        list.appendChild(deleteButton);
         list.appendChild(anchor);
     }
 }
